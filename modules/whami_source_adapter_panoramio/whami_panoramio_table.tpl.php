0b81770bfe91503fe6ca00c7d7419623ee4c6112
vlad.k <vlad.kraeuter@gmail.com>
2012-06-30 19:27:05 +0200

    Cleanup before promoting project.

--- modules/whami_source_adapter_panoramio/whami_panoramio_table.tpl.php
+++ modules/whami_source_adapter_panoramio/whami_panoramio_table.tpl.php
@@ -6,8 +6,13 @@
 
 ?>
 <div align="center" id='whamiPanoramioPicsAround'>
-  <p id='whamiPanoramioTitle'><?php print $table_title; ?></p>
+
   <?php echo $html_table; ?>
-  <div id='whamiPanoramioLogo'><?php print $panoramio_logo; ?></div>
-  <p id='whamiPanoramioLicence'><?php print $panoramio_license; ?></p>
+
+    <p class='whamiPanoramioLicence'><?php print $panoramio_title; ?></p>
+
+    <div id='whamiPanoramioLogo'><?php print $panoramio_logo; ?></div>
+
+    <p class='whamiPanoramioLicence'><?php print $panoramio_license; ?></p>
+
 </div>
